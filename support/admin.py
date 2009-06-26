a08257fa3c9c5e46a0c8a8cb9b4bbe730cf1de3c
Philipp Bosch <pb@philippbosch.de>
2009-06-26 17:02:26 +0200

    supporters filterable by verified status

--- support/admin.py
+++ support/admin.py
@@ -5,5 +5,6 @@ from support.models import Supporter
 
 class SupporterAdmin(admin.ModelAdmin):
     list_display = ('__unicode__', 'strasse', 'plz', 'ort', 'verified', 'date_registered',)
+    list_filter = ('verified',)
 
 admin.site.register(Supporter, SupporterAdmin)
